32b2fca89716f11c2f4f066cacdcd58cb9f066ab
tanmaygaikwad04d-lang <tanmaygaikwad04d@gmail.com>
2026-09-10 21:24:36 +0530

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  float b;
+  double c;
+  char ch;
+  char name[50];
+
+  printf("Enter an integer:");
+  scanf("%d", &a);
+
+  printf("Enter a float value:");
+  scanf("%f", &b);
+
+  printf("Enter a double value:");
+  scanf("%lf", &c);
+
+  printf("Enter a single character:");
+  scanf("%c", &ch);
+
+  printf("Enter a string(name):");
+  scanf("%s", name);
+
+  printf("integer entered : %d\n",a);
+  printf("flloat entered : %.2f\n",b);
+  printf("double entered : %.2lf\n",c);
+  printf("character entered : %c\n",ch);
+  printf("string entered : %s\n",name);
+
+  return 0;
+}  
+
