afaecac4de32d4246e79ee59ec1f01116b80a64f
tanmaygaikwad04d-lang <tanmaygaikwad04d@gmail.com>
2026-09-12 11:17:33 +0530

    Add files via upload

---
+++ 05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+ // This is a single-line comment
+ // It explains that the program prints a welcome message
+ /* This is a multi- line comment.
+ It can span across all lines.
+ Here, we declare a variable and print its value. */
+ int num = 10; // Single-line comment: declaring and initialising a variable
+ printf("The value of num is: %d\n", num);
+ /* End the program.
+ Comments do not affect program execution;
+ they are purely for documentation purposes. */
+ return 0;   
+}
